6c22ff2300643fcd0015fe10a15380be57c4ad43
leslieleefook <129245472+leslieleefook@users.noreply.github.com>
2026-01-26 13:07:17 -0400

    Remove duplicate airoi project - tests were running twice

--- playwright.config.js
+++ playwright.config.js
@@ -21,10 +21,6 @@ module.exports = defineConfig({
         {
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
-        },
-        {
-            name: 'airoi',
-            use: { ...devices['Desktop Chrome'] },
         }
     ],
 });
